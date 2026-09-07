ed6575f18904cefccb1452117f1713e51d6bb8e9
cksgud9831-max <cksgud9831@gmail.com>
2026-09-07 12:13:18 +0900

    chore: 중복된 ros2_gazebo 사본 제거 (simulation/ciws_turret_aerial_object_detection-main 단일화)

--- simulation/ros2_gazebo/drone_sim/drone_sim/smash_fcs/__init__.py
+++
@@ -1,9 +0,0 @@
-"""SMASH FCS 지원 모듈: 포탑 기구학, 거리 추정, 조준 코어 부트스트랩.
-
-ROS 2 의존성이 없는 순수 파이썬 모듈만 모아 두었다. 덕분에 ROS 없이도
-scripts/08_stage1_fcs_offline_validation.py 로 단독 검증할 수 있다.
-"""
-
-from . import range_estimator, turret_kinematics
-
-__all__ = ["turret_kinematics", "range_estimator"]
